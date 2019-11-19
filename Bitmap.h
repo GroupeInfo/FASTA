b20a7591c321c1ee280b26076536be88a2621cdd
HassanNehme <44973835+suckmylollipop@users.noreply.github.com>
2019-11-19 13:26:10 +0100

    Add files via upload

---
+++ Bitmap.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+map<string,string> create_Bitmap();
---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include "Bitmap.h"
+#define BINSEPARATOR 8;
+using namespace std;
+
+void printout(const string& s, ofstream& out);
+void transform(const map<string, string>& m, const string& c, string& s);
+void dostuff(const map<string,string>& liste, ifstream& finput, ofstream& foutput);
+void compare( ifstream& database, ifstream& query);
+
+int main(int argc, char* argv[]){
+	ifstream inFile;
+	inFile.open(argv[1]);
+	ofstream outFile;
+	outFile.open(argv[2]);
+	ifstream fileIn;
+	fileIn.open(argv[3]);
+	map<string,string> Bitmap = create_Bitmap();
+	
+	if(inFile.fail() || outFile.fail() || fileIn.fail()){
+		cerr<< "Error Opening file" << endl;
+		exit(1);
+		}
+	string q = dostuff(Bitmap,inFile, outFile);
+	
+	ifstream bitFile;
+	bitFile.open(argv[2]);
+	compare(fileIn,q);
+	return 0;
+}
+
+
+
+void transform(const map<string,string>& m, const string& c, string& s){
+	for(map<string,string>::const_iterator itr = m.begin(); itr!=m.end(); itr++){
+		if(itr->first==c){
+			s.append(itr->second);
+		}															
+	}
+};
+
+void printout(const string& s, ofstream& out){
+	out<<s<<endl;
+	out.close();
+	}
+
+string dostuff(const map<string,string>& liste, ifstream& finput, ofstream& foutput){
+		char sh;
+		string str ="";
+		while( !finput.eof()){
+			finput >> ch;
+			string sh ="";
+			sh.append(1,ch);
+			transform(liste, sh, str);
+			}
+		finput.close();
+		printout(str,foutput);
+		//cout<<str<<endl;
+		return str;
+}
+
+bool compare(ifstream& database, string query){
+	char c;
+	int bin[150];
+	int counter = 0;
+	int* pa = NULL;
+	int a = 0;
+	while( !database.eof()){
+		++counter;
+		finput >> c;
+		int binary = (int)c
+		bin[counter] = binary;
+		pa = &bin[counter];
+		if(counter>=BINSEPARATOR){
+			for(int i =0; i<BINSEPARATOR<++i){
+				if(*(pa-i)== 0){
+					++a;
+				}
+			}
+		}
+		if(a==8){
+			int sequence_length = counter-BINSEPARATOR-1;
+			string s ="";
+			for(int j = 0;j<sequence_length<j++){
+				s.append(1,bin[j];
+			}
+			if(s==query){
+				return true;
+			}
+			else{
+				counter = 0;
+				}
+			
+		}
+		else{
+			a=0;
+		}
+	}
+}
+
+	
+
+
+
+
