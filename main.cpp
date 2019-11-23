72bf993b421029afcb13760505be2b03671a7498
HassanNehme <44973835+suckmylollipop@users.noreply.github.com>
2019-11-23 13:21:58 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,111 +1,93 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <map>
-#include "Bitmap.h"
-#define BINSEPARATOR 8
 using namespace std;
-
-void printout(const string& s, ofstream& out);
-void transform(const map<string, string>& m, const string& c, string& s);
-string dostuff(const map<string,string>& liste, ifstream& finput, ofstream& foutput);
-int compare( ifstream& database, string query);
-
-int main(int argc, char* argv[]){
-	ifstream inFile;
-	inFile.open(argv[1]);
-	ofstream outFile;
-	outFile.open(argv[2]);
-	ifstream fileIn;
-	fileIn.open(argv[3]);
-	map<string,string> Bitmap = create_Bitmap();
+int main(){
+	//int titlelength;
+	//char timeStampLength; 
+	//char timeStamp;
+	//char numberOfSequences;
+	//char maximumLength;
+	//char headerOffsetTable;
+	//char sequenceOffsetTable;
+	//char ambiguityOffsetTable;
+	int N;
+	ifstream f ("uniprot_sprot.fasta.pin", ios::out|ios ::binary);
 	
-	if(inFile.fail() || outFile.fail() || fileIn.fail()){
-		cerr<< "Error Opening file" << endl;
-		exit(1);
+	if(!f.is_open()){
+		cout<<"impossible d'ouvrire le fichier !"<<endl;
 		}
-	string q = dostuff(Bitmap,inFile, outFile);
-	
-	//ifstream bitFile;
-	//bitFile.open(argv[2]);
-	int foo = compare(fileIn,q);
-	cout<<foo<<endl;
+	else{
+		uint32_t version ;
+		f.read((char*)&version,sizeof(uint32_t));
+		version = __builtin_bswap32(version);
+		cout<<"Version : " << version<<endl;
+		
+		uint32_t databaseType ;
+		f.read((char*)&databaseType,sizeof(uint32_t));
+		databaseType = __builtin_bswap32(databaseType);
+		cout<<"DatabaseType : " << databaseType<<endl;
+		
+		uint32_t titleLength ;
+		f.read((char*)&titleLength,sizeof(uint32_t));
+		titleLength = __builtin_bswap32(titleLength);
+		cout<<"Title length : " << titleLength<<endl;	
+		
+		char titleString[titleLength];
+		for(unsigned int i=0; i<titleLength; ++i){
+			f.read((char*)&titleString[i], sizeof(char));
+		}
+		cout<<"Title string : "<<titleString<<endl;
+		
+		uint32_t timestampLength ;
+		f.read((char*)&timestampLength,sizeof(uint32_t));
+		timestampLength = __builtin_bswap32(timestampLength);
+		cout<<"timestamp length : " << timestampLength<<endl;
+		
+		char timestamp[timestampLength];
+		for(unsigned int i=0; i<timestampLength; ++i){
+			f.read((char*)&timestamp[i], sizeof(char));
+		}
+		cout<<"timestamp : "<<timestamp<<endl;
+		
+		uint32_t sequencesNumber ;
+		f.read((char*)&sequencesNumber,sizeof(uint32_t));
+		sequencesNumber = __builtin_bswap32(sequencesNumber);
+		cout<<"Number of sequences : " << sequencesNumber<<endl;
+		N = sequencesNumber;
+		
+		uint64_t residuesNumber ;
+		f.read((char*)&residuesNumber,sizeof(uint64_t));
+		cout<<"Total number of residues : " << residuesNumber<<endl;
+		
+		uint32_t MaximumSequence ;
+		f.read((char*)&MaximumSequence,sizeof(uint32_t));
+		MaximumSequence = __builtin_bswap32(MaximumSequence);
+		cout<<"Maximum Sequence: " << MaximumSequence<<endl;
+		
+		uint32_t HeaderOffsetTable[N+1];
+		for(unsigned int i=0; i<N+1; ++i){
+			f.read((char*)&HeaderOffsetTable[i], sizeof(uint32_t));
+		}
+		for(unsigned int i=0; i<N+1; ++i){
+			cout<<"header Offset: " << HeaderOffsetTable[i]<<endl;
+		}
+		
+		uint32_t sequenceOffsetTable[N+1];
+		for(unsigned int i=0; i<N+1; ++i){
+			f.read((char*)&sequenceOffsetTable[i], sizeof(uint32_t));
+		}
+		int counter = 0;
+		int off = 20;
+		bool b = false;
+		for(unsigned int i=0; i<N+1; ++i){
+			++counter;
+			cout<<"Sequence Offset: " << sequenceOffsetTable[i]<<endl;
+			if(sequenceOffsetTable[i] == off)
+				b = true;
+		}
+		cout<<"je t'ai affiché"<<counter<<" offset de sequences"<<endl;
+		cout<<b<<endl;
+	}
 	return 0;
-}
-
-
-
-void transform(const map<string,string>& m, const string& c, string& s){
-	for(map<string,string>::const_iterator itr = m.begin(); itr!=m.end(); itr++){
-		if(itr->first==c){
-			s.append(itr->second);
-		}															
-	}
 };
-
-void printout(const string& s, ofstream& out){
-	out<<s<<endl;
-	out.close();
-	}
-
-string dostuff(const map<string,string>& liste, ifstream& finput, ofstream& foutput){
-		char ch;
-		string str ="";
-		while( !finput.eof()){
-			finput >> ch;
-			string sh ="";
-			sh.append(1,ch);
-			transform(liste, sh, str);
-			}
-		finput.close();
-		printout(str,foutput);
-		return str;
-}
-
-int compare(ifstream& database, string query){
-	char c;
-	string bin[20];
-	int counter = -1;
-	int counter2 = 0;
-	string* pa = NULL;
-	bool foo = false;
-	string swr = "";
-	while( !database.eof()){
-		++counter;
-		database >> c;
-		swr.append(1,c);
-		if(counter==7){
-			bin[counter2] = swr;
-			pa = &bin[counter2];
-			cout<<bin[counter2]<<endl;
-			++counter2;
-			counter = -1;
-			if(swr=="00000000"){
-				foo = true;
-				}
-			swr="";
-		}
-		if(foo==true){
-			int sequence_length = counter2-1;
-			string s ="";
-			for(int j = 0; j<sequence_length; j++){
-				s+=bin[j];
-			}
-			if(s==query){
-				cout<<"wohoooo"<<endl;
-				return 1;
-			}
-			else{
-				counter2=0;
-				foo = false;
-				}
-			
-		}
-	}
-}
-
-	
-
-
-
-
