05e2bb6c5fa4db7faf57a45e38d22d832fb6ab95
HassanNehme <44973835+GroupeInfo@users.noreply.github.com>
2019-12-16 10:01:23 +0100

    Add files via upload

---
+++ protein.cpp
@@ -0,0 +1,34 @@
+#include "protein.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+Protein::Protein() {
+	
+}
+
+Protein::Protein(char* sequence, int length, string header) {
+	this->length = length;
+	this->sequence = sequence;
+	this->header = header;
+}
+
+Protein::~Protein() {}
+
+void Protein::setSequence(char* sequence, int length) {
+	this->length = length;
+	this->sequence = sequence;
+}
+
+const char Protein::getResidue(int i) const {
+	return sequence[i];
+}
+
+char* Protein::getSequence() const {
+	return sequence;
+}
+
+void Protein::setHeader(string header) {
+	this->header = header;
+}
