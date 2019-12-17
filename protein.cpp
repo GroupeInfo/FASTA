b5fa7a3ed45bafb526b89fa2fb23c6755a2d9f5f
HassanNehme <44973835+GroupeInfo@users.noreply.github.com>
2019-12-17 10:10:14 +0100

    Add files via upload

--- protein.cpp
+++ protein.cpp
@@ -33,13 +33,12 @@ void Protein::setHeader(string header) {
 	this->header = header;
 }
 
-void Protein::print(ostream& out) { //only print the header  //if there is already a >, don't print another !
-	out << ">";  //this is because of weird caracters at the begining (can't figure out why) -> solution : skip the 's' when reading the header
+void Protein::print(ostream& out) { 
+	out << ">";  
 	out << "s";
 	out << header << "\n";
 }
 
 const int Protein::size() const {
-	//return the lenght of the sequence
 	return length;
 }
--- protein.h
+++ protein.h
@@ -8,24 +8,19 @@ class Protein {
 	protected :
 		char* sequence;
 		string header;
-		int length; //length of the sequence
-		//return a sequence string in readable format (letters instead of binary)
+		int length;
 	
 	public :
 	
 		Protein();
 		Protein(char* sequence, int length, string header);
 		~Protein();
-		//load the header and the sequence of a protein from a fasta file containing only one protein
 		char* getSequence() const;
 		void setSequence(char* sequence, int length);
 		string getHeader();
 		void setHeader(string header);
 		const char getResidue(int i) const;
-		//return the lenght of the sequence
 		const int size() const;
 		void print(std::ostream & out = std::cout);
-		//define the == operator for Protein --> we can do : proteinA == proteinB
-		//two proteins are equal if they have the same sequence
 	
 };
