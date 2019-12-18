2ae9b4606e8fb10739bf5eb6a382458a2b7c9614
PyrDeus <49350315+PyrDeus@users.noreply.github.com>
2019-12-18 09:29:29 +0100

    Add files via upload

--- database.cpp
+++ database.cpp
@@ -7,166 +7,155 @@
 using namespace std;
 
 Database::Database(string filename) {
-	
+	//== Constructeur ==//
 	this->filename = filename;
-	loadData(filename);
-	printInfos();
-	loadProteins(filename);
-	loadHeaders(filename);
+	data(filename);
+	print();
+	proteins(filename);
+	headers(filename);
 }
 
-void Database::loadData(const string &filename) {
-	
+void Database::data(const string &filename) {
+	//== Partie d'extraction d'information du fichier .pin ==//
 	ifstream pinFile;
 	pinFile.open(filename + ".pin", ios::binary | ios::in);
-
-	if (pinFile.is_open()) {
-		
-		//version 
-		pinFile.read((char*)&version,sizeof(uint32_t));
-		version = __builtin_bswap32(version);
-		
-		//databaseType
-		pinFile.read((char*)&databaseType,sizeof(uint32_t));
-		databaseType = __builtin_bswap32(databaseType);
-		cout<<"The database type is " << databaseType<<endl;
-		
-		//titleLength
-		pinFile.read((char*)&titleLength,sizeof(uint32_t));
-		titleLength = __builtin_bswap32(titleLength);	
-		
-		//title
-		char titleString[titleLength];
-		for(unsigned int i=0; i<titleLength; ++i){
-			pinFile.read((char*)&titleString[i], sizeof(char));
-		}
-		title = (string)titleString;
-		//timestampLength
-		pinFile.read((char*)&timestampLength,sizeof(uint32_t));
-		timestampLength = __builtin_bswap32(timestampLength);
-		
-		//timestamp
-		char timestampcopy[timestampLength];
-		for(unsigned int i=0; i<timestampLength; ++i){
-			pinFile.read((char*)&timestampcopy[i], sizeof(char));
-		}
-		timestamp = string(timestampcopy);
-		
-		//Number of sequences
-		pinFile.read((char*)&nbrSequences,sizeof(uint32_t));
-		nbrSequences = __builtin_bswap32(nbrSequences);
-		
-		//Number of residues
-		pinFile.read((char*)&nbrResidues,sizeof(uint64_t));
-		
-		//Maximum sequence
-		pinFile.read((char*)&maxSequence,sizeof(uint32_t));
-		maxSequence = __builtin_bswap32(maxSequence);
-		
-		//header offset table
-		headerOffsets.reserve(nbrSequences+1); 		//fix the size of the vector
-		uint32_t offset; 						//the offset is get as 4 bytes before convert it into an integer
-		for (unsigned int i = 0; i <= nbrSequences; i++) {
-			pinFile.read((char*) &offset, sizeof(uint32_t));
-			headerOffsets.push_back(__builtin_bswap32(offset)); //convert bytes and store them as integer
-		}	
-		
-		//sequence offset table
-		sequenceOffsets.reserve(nbrSequences+1); 	//fix the size of the vector
-		for (unsigned int i = 0; i <= nbrSequences; i++) {
-			pinFile.read((char*) &offset, sizeof(uint32_t));
-			sequenceOffsets.push_back(__builtin_bswap32(offset)); //convert bytes and store them as integer
-		}
-		pinFile.close();		
-	}
-	else {
-		cout << "Unable to open database file : " << filename << ".pin\n";
+	
+	// Vérification d'erreur d'ouverture
+	if (!pinFile.is_open()) {
+		cout << "Erreur ouverture de " << filename << ".pin\n";
 		exit(1);
 	}
+	
+	//--Extraction--//
+	
+	//-Version-// 
+	pinFile.read((char*)&version,sizeof(uint32_t));
+	version = __builtin_bswap32(version);
+	
+	//-DatabaseType-//
+	pinFile.read((char*)&databaseType,sizeof(uint32_t));
+	databaseType = __builtin_bswap32(databaseType);
+	
+	//-TitleLength//
+	pinFile.read((char*)&titleLength,sizeof(uint32_t));
+	titleLength = __builtin_bswap32(titleLength);	
+	
+	//-Title-//
+	char titleString[titleLength];
+	for(int i=0; i<titleLength; ++i){
+		pinFile.read((char*)&titleString[i], sizeof(char));
+	}
+	title = (string)titleString;
+	
+	//-TimestampLength-//
+	pinFile.read((char*)&timestampLength,sizeof(uint32_t));
+	timestampLength = __builtin_bswap32(timestampLength);
+	
+	//-Timestamp-//
+	char timestampcopy[timestampLength];
+	for(int i=0; i<timestampLength; ++i){
+		pinFile.read((char*)&timestampcopy[i], sizeof(char));
+	}
+	timestamp = string(timestampcopy);
+	
+	//-NbrSequences-//
+	pinFile.read((char*)&nbrSequences,sizeof(uint32_t));
+	nbrSequences = __builtin_bswap32(nbrSequences);
+	
+	//-NbrResidues-//
+	pinFile.read((char*)&nbrResidues,sizeof(uint64_t));
+	
+	//-MaxSequence-//
+	pinFile.read((char*)&maxSequence,sizeof(uint32_t));
+	maxSequence = __builtin_bswap32(maxSequence);
+	
+	//-HeaderOffsetTable-//
+	headerOffsets.reserve(nbrSequences+1); 		//fix the size of the vector
+	uint32_t offset; 						//the offset is get as 4 bytes before convert it into an integer
+	for (int i = 0; i <= nbrSequences; i++) {
+		pinFile.read((char*) &offset, sizeof(uint32_t));
+		headerOffsets.push_back(__builtin_bswap32(offset)); //convert bytes and store them as integer
+	}	
+	
+	//-SequenceOffsetTable-//
+	sequenceOffsets.reserve(nbrSequences+1); 	//fix the size of the vector
+	for (int i = 0; i <= nbrSequences; i++) {
+		pinFile.read((char*) &offset, sizeof(uint32_t));
+		sequenceOffsets.push_back(__builtin_bswap32(offset)); //convert bytes and store them as integer
+	}
+	pinFile.close();
 }
 
-void Database::loadProteins(const string &filename) {
-	//load sequences from file
+void Database::proteins(const string &filename) {
+	//== Partie d'extraction d'informations du fichier .psq ==//
+	ifstream psqFile;
+	psqFile.open(filename + ".psq", ios::binary | ios::in);
+	psqFile.seekg(0, ios::beg);
 	
-	if (!headerOffsets.empty() && !sequenceOffsets.empty()) {
-		
-		ifstream psqFile;
-		psqFile.open(filename + ".psq", ios::binary | ios::in);
-		psqFile.seekg(0, ios::beg);
-		
-		if (psqFile.is_open()) {
-	
-			int sequenceSize;
-			for (unsigned int i = 0; i <= nbrSequences-1 ; i++){  //nbrSequences + 1
-				
-				char firstByte[1];  
-				psqFile.read((char*) &firstByte, sizeof(firstByte));  // go one byte forward to avoid the null byte at the beginning
-				
-				sequenceSize = sequenceOffsets[i+1] - sequenceOffsets[i] -1  ;
-				char* sequence = new char[sequenceSize];
-				psqFile.read(sequence, sequenceSize);
-				//cout<<"sequence : "<<sequence<<endl;
-				Protein * prot = new Protein();
-				prot->setSequence(sequence, sequenceSize);
-				proteins.push_back(*prot);
-			}
-		}
-		else {
-			cout << "Unable to open database file : " << filename << ".psq\n";
-			throw string("Unable to load file");
-		}
+	// Vérification d'erreur d'ouverture
+	if (!psqFile.is_open()) {
+		cout << "Erreur Ouverture de " << filename << ".psq\n";
+		exit(1);
 	}
-	else cout << "Header offsets or sequence offsets not yet loaded\n";
+	//--Extraction--//
+	int sequenceLength;
+	for (int i = 0; i < nbrSequences ; i++){
+		
+		char firstByte[1];  
+		psqFile.read((char*) &firstByte, sizeof(firstByte));
+		
+		sequenceLength = sequenceOffsets[i+1] - sequenceOffsets[i] -1  ;
+		char* sequence = new char[sequenceLength];
+		psqFile.read(sequence, sequenceLength);
+		Protein * prot = new Protein();
+		prot->setSequence(sequence, sequenceLength);
+		proteins.push_back(*prot);
+	}
+	psqFile.close();
 }
 
-void Database::loadHeaders(const string &filename) {
-	//load headers from file
+void Database::headers(const string &filename) {
+	//== Partie d'extraction d'informations du fichier .phr ==//
 	
-	if (headerOffsets.empty() && sequenceOffsets.empty()) {
-		cout << "Header offsets or sequence offsets not yet loaded\n";
-	}
-		
 	ifstream phrFile;
 	phrFile.open(filename + ".phr", ios::binary | ios::in);
 	phrFile.seekg(1, ios::beg);
-		
+	
+	// Vérification d'erreur d'ouverture
 	if (!phrFile.is_open()) {
-		cout << "Unable to open database file : " << filename << ".phr\n";
+		cout << "Erreur ouverture de " << filename << ".phr\n";
 		exit(1);
 	}
-	
-	int headerSize;
-	for (unsigned int i = 0; i < nbrSequences ; i++){  
-				
-		headerSize = headerOffsets[i+1] - headerOffsets[i];
-		char header[headerSize];
-		phrFile.read((char*) &header, headerSize);
-				
+	//--Extraction--//
+	int headerLength;
+	for (int i = 0; i < nbrSequences ; i++){  
+		
+		headerLength = headerOffsets[i+1] - headerOffsets[i];
+		char header[headerLength];
+		phrFile.read((char*) &header, headerLength);
+		
 		proteins[i].setHeader(string(header).substr(8, string::npos));
 	}
+	phrFile.close();
 }
 
 Protein & Database::getProtein(unsigned int index) {
-	if (index < nbrSequences) {
-		return proteins[index];
-	}
-	return proteins[nbrSequences-1]; //if index not valid, return last protein
+	return proteins[index];
 }
 
-void Database::printInfos(std::ostream & out) {
+void Database::print(std::ostream & out) {
+	//== Print les infos contenue dans la database ==//
 	
-	out << "===== Database informations =====" << endl;
-	out << " - Version : " <<  version << "\n";
-	out << " - Type : " << databaseType  << "\n";
-	//out << " - Title lenght : " << titleLength  << "\n";
-	out << " - Title : " << title << "\n";
-	//out << " - Timestamp Length : " <<  timestampLength  << "\n";
-	out << " - Timestamp : " << timestamp  << "\n";
-	out << " - Number of sequences : "  <<  nbrSequences << "\n";
-	out << " - Residue count : " <<  nbrResidues << "\n";
-	out << " - Longuest sequence : " << maxSequence << "\n";
-	out << " ================================ " << endl;
-	out<<endl;
+	out << " ===== Database =====" << endl;
+	out << " - Version : " <<  this->version << endl;
+	out << " - Type de la Database : " << this->databaseType  << endl;
+	out << " - Titre : " << this->title << endl;
+	out << " - Timestamp : " << this->timestamp  << endl;
+	out << " - Nombre de Séquences : "  <<  this->nbrSequences << endl;
+	out << " - Nombre de Residues : " <<  this->nbrResidues << endl;
+	out << " - Plus Longue Séquence : " << this->maxSequence << endl;
+	out << " ====================" << endl;
+	out<< "\n" <<endl;
 }
 
 int Database::getNbrSequences() {
--- main.cpp
+++ main.cpp
@@ -1,11 +1,80 @@
+#include "database.h"
+#include "scorematrix.h"
 #include <algorithm>
 #include <iostream>
-#include <utility>
+#include <time.h>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+void gotoh(Database& data, ScoreMatrix& blosum, Query& query, int protIndex, int ext_penalty, int sum_penalty) {
 	
+	Protein& prot = data.getProtein(protIndex);
+	
+	//Matrice MxN
+	const unsigned int n = query.size(); // Taille max colonne
+	const unsigned int m = prot.size(); // Taille max ligne
+	
+	// ext_penalty = R
+	// sum_penalty = Q
+	
+	int H[m+1];
+	int F[m+1];
+	
+	int H_diag;
+	int H_diag_buffer;
+	int E;
+	int score = 0; // return 0 si H[i,j] toujours < 0
+	
+	//-Initialisation H et F-//
+	// Eviter les pointeurs fous
+	for (unsigned int j = 0; j <= m; j++) {
+		H[j] = 0;
+		F[j] = 0; // Valeur de fin de récurrence de F
+	}
+	
+	for (unsigned int i = 1; i <= n; i++) {
+		// Valeurs de fin de récurrence de E et H
+		E = 0; // E = E[i,j]
+		H_diag = 0; // H_diag = H[i-1,j-1]
+		//cout << "H_diag0 = " << H_diag << endl;
+		//cout << "E0 = " << E << endl;
+		
+		for (unsigned int j = 1; j <= m; j++) {
+			// E[i,j] = max(H[i,j-1] - Q, E[i,j-1] - R)
+			E = max(H[j-1] - sum_penalty,  E - ext_penalty);
+			// F[i,j] = max(H[i-1,j] - Q, F[i-1,j] - R)
+			F[j] = max(H[j] - sum_penalty, F[j] - ext_penalty);
+			
+			H_diag_buffer = H[j];
+			cout << "H_diag_buffer = " << H_diag_buffer << endl;
+			// H[i,j] = max(H[i-1,j-1] + BLOSUM62, E[i,j], F[i,j], 0)
+			H[j] = max( max( H_diag + (int) blosum(prot.getResidue(j-1), (int) query.getResidue(i-1)), E ), max( F[j], 0 ));
+			
+			H_diag = H_diag_buffer; // H[i-1,j-1] sauvé pour la prochaine itération
+			
+			cout << "H_diag = " << H_diag << endl;
+			cout << "E = " << E << endl;
+			
+			cout << "H[j] = " << H[j] << endl;
+			cout << "score = " << score << endl;
+			// score = max(H[i,j]) où 0<=i<=n et 0<=j<=m
+			if (H[j] > score) {
+				score = H[j];
+			}
+		}
+	}
+	
+}
+
+int main(int argc, char* argv[]) {
+	/* utilisation de time_t ou alors compilation en utilisant time lors de l'essais :
+	 * time ./gotoh uniprot_sprot.fasta P00533.fasta BLOSUM62.txt 11 1
+	 * ou
+	 * time ./gotoh newE.fasta P00533.fasta BLOSUM62.txt 11 1 */
+	time_t start, end;
+	time(&start);
+	
+	// Vérification Arguments
 	if (argc != 6) { // argv[0] est le nom de la fonction
 		cout << "5 arguments attendus : [database] [query_protein] [blosum62_matrix] [gap_penalty] [extend_gap_penalty]" << endl;
 		return EXIT_FAILURE;
@@ -17,8 +86,10 @@ int main(int argc, char* argv[]) {
 	int penalty = atoi(argv[4]);
 	int ext_penalty = atoi(argv[5]);
 	
+	//== Initialisation Database ==//
 	Database data = Database(database_path);
 	
+	//== Initialisation Query Protein ==//
 	Query query;
 	query.loadFromFile(protein_path);
 	cout << "===== Query Protein =====" << endl;
@@ -26,10 +97,15 @@ int main(int argc, char* argv[]) {
 	cout << "=========================" << endl;
 	cout << "\n" << endl;
 	
+	//== Initialisation Blosum Matrix ==//
+	ScoreMatrix blosum = ScoreMatrix(blosum_path);
 	
-	int nbrSequences = data.getNbrSequences();
-	vector<Protein> proteines; // contient toutes les proteines
-	proteines.resize(nbrSequences); // nbrSequence = max(index)
+	int sum_penalty = ext_penalty + penalty;
 	
+	gotoh(Database& data, ScoreMatrix& blosum, Query& query, int protIndex, int ext_penalty, int sum_penalty);
+	
+	time(&end);
+	int t_exec = difftime(end, start);
+	cout << "Temps d'éxecution : " << t_exec << " secondes" << endl;
 	return 0;
 }
